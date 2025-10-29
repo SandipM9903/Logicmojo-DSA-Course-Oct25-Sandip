cb54af44fea9c8decd5bc2a8df4ee2be1ec10094
Sandip Mondal <72807288+SandipM9903@users.noreply.github.com>
2025-10-29 11:14:13 +0530

    feat: Solve Remove Duplicates from Sorted Array

---
+++ RemoveDuplicateFromSortedArray.java
@@ -0,0 +1,21 @@
+// Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same.
+
+public class RemoveDuplicateFromSortedArray{
+  public int removeDuplicate(int[] nums){
+    if(nums.length == 0) return 0;
+
+    int i = 0;
+    for(int j = 1; j < nums.length; j++){
+      if(nums[j] != nums[i]){
+        i++;
+        nums[i] = nums[j];
+      }
+    }
+    return i + 1;
+  }
+  public static void main(String[] args){
+    int[] nums = {1,1,2,2,3,3};
+    RemoveDuplicateFromSortedArray solution = new RemoveDuplicateFromSortedArray();
+    System.out.println(solution.removeDuplicate(nums));
+  }
+}
