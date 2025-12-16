67cf0f983d51550cd64da89ad5f55d40246ace56
SandipM9903 <sandipmondal990355@gmail.com>
2025-12-16 21:55:58 +0530

    Soduko Solver Done

--- Practise/Recursion/SudokuSolver.java
+++ Practise/Recursion/SudokuSolver.java
@@ -1,17 +1,68 @@
 package Practise.Recursion;
 
 public class SudokuSolver {
+    //Third we need to implement isSafe()
+    public boolean isSafe(char[][] board, int row, int col, int digit){
+        //Horizontal
+        for(int i=0; i<9; i++){
+            if(board[row][i] == digit){
+                return false;
+            }
+        }
+
+        //Vertical
+        for(int i=0; i<9; i++){
+            if(board[i][col] == digit){
+                return false;
+            }
+        }
+
+        //grid
+        int srow = (row/3) * 3;
+        int scol = (col/3) * 3;
+
+        for(int i=srow; i<=srow+2; i++){
+            for(int j=scol; j<=scol+2; j++){
+                if(board[i][j] == digit){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 
     public boolean helper(char[][] board, int row, int col){
 
+        //Fourth we need to check the base condition
+        if(row == 9){
+            return true;
+        }
 
-        for(int i=1; i<=9; i++){
+        //First we need to check whether any digit is present in the board[row][col]
+        int nextRow = row, nextCol = col+1;
+        if(nextCol == 9){
+            nextRow = row+1;
+            nextCol = 0;
+        }
 
+        if(board[row][col] != '.'){
+            return helper(board, nextRow, nextCol);
+        }
+
+        //Second we need to place the digit
+        for(char i='1'; i<='9'; i++){
+            if(isSafe(board, row, col, i)){
+                board[row][col] = i;
+                if(helper(board, nextRow, nextCol)){
+                    return true;
+                }
+                board[row][col] = '.';
+            }
         }
         return false;
     }
 
     public void solveSudoku(char[][] board) {
-
+        helper(board, 0, 0);
     }
 }
