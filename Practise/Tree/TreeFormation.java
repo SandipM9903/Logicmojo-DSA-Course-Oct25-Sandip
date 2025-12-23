dfee72a2715d357f9aff0774721fd06d8b1ca03c
SandipM9903 <sandipmondal990355@gmail.com>
2025-12-23 12:47:21 +0530

    Preorder traversal

--- Practise/Tree/TreeFormation.java
+++ Practise/Tree/TreeFormation.java
@@ -36,10 +36,26 @@ public class TreeFormation {
         }
     }
 
+    //Preorder traversal
+    public static void preorder(Node root){
+        //Base case
+        if(root == null){
+            return;
+        }
+        //First we need to print the root data
+        //Then left data
+        //Then right data
+        System.out.print(root.data + " ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+
     public static void main(String[] args) {
         int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
-        System.out.println(root.data);
+//        System.out.println(root.data);
+        System.out.println("Preorder Traversal");
+        preorder(root);
     }
 }
