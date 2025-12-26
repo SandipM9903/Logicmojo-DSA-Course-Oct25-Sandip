e6c96580842a4b9e46bd193b1120447f8bbfaa64
SandipM9903 <sandipmondal990355@gmail.com>
2025-12-26 18:09:55 +0530

    Level Order Traversal

---
+++ Practise/Tree/LevelOrderTraversal.java
@@ -0,0 +1,71 @@
+package Practise.Tree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class LevelOrderTraversal {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static class BuildBinaryTree{
+        static int index = -1;
+        public static Node buildTree(int[] nodes){
+            index++;
+            if(nodes[index] == -1){
+                return null;
+            }
+
+            Node newNode = new Node(nodes[index]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+            return newNode;
+        }
+    }
+
+    //Now Implementing Level Order Traversal
+    public static void lot(Node root){
+        if(root == null){
+            return;
+        }
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        q.add(null);
+
+        while(!q.isEmpty()){
+            Node currNode = q.remove();
+            if(currNode == null){
+                System.out.println();
+                if(q.isEmpty()){
+                    break;
+                } else {
+                    q.add(null);
+                }
+            } else {
+                System.out.print(currNode.data + " ");
+                if(currNode.left != null){
+                    q.add(currNode.left);
+                }
+                if(currNode.right != null){
+                    q.add(currNode.right);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+        Node root = BuildBinaryTree.buildTree(nodes);
+        System.out.println(root.data);
+        System.out.println("==== Level Order Traversal ====");
+        lot(root);
+    }
+}
